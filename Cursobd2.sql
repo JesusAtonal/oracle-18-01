a5cbbfb6b70a4c84299201503879921bfbd378ac
JesusAtonal <31869868+JesusAtonal@users.noreply.github.com>
2017-10-11 21:46:08 -0500

    Add files via upload

--- Cursobd2.sql
+++ Cursobd2.sql
@@ -6,6 +6,36 @@ create table almacen(numero_almacen integer,
                     constraint pk_na primary key (numero_almacen)
                     );
   
+  
+  create table vendedor(numero_vendedor integer,
+                        nombre_vendedor varchar2(60),
+                        area_ventas varchar2(60),
+                        constraint pk_nv primary key(numero_vendedor)
+                        );
+                        
+DESCRIBE almacen;
+describe vendedor;
+ 
+ 
+ create table cliente(numero_cliente integer,
+                      numero_almacen integer,
+                      nombre_cliente varchar2(60),
+                      constraint pk_nc primary key(numero_cliente),
+                      constraint fk1_na foreign key (numero_almacen)
+                      references almacen(numero_almacen)
+                      );
+                      
+create table ventas(id_ventas integer,
+                    numero_cliente integer,
+                    numero_vendedor integer,
+                    monto_venta float,
+                    constraint pk_Vv1 primary key (id_ventas),
+                    constraint fk_nc1 foreign key (numero_cliente)
+                    references cliente(numero_cliente),
+                    constraint fk_nv1 foreign key (numero_vendedor)
+                    references vendedor(numero_vendedor)
+                    );
+  
   --Vamos a generar un procedimiento almacenado para guardar
   --una entidad o registro de tipo almacen
   
